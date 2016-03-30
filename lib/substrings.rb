cb01a2b01074889a87faf89c3c62077ccded1cec
Kate Flood <kathryneflood@gmail.com>
2016-03-29 21:04:32 -0500

    updated regex

--- lib/substrings.rb
+++ lib/substrings.rb
@@ -1,3 +1,17 @@
-class substrings
+class Substrings
+
+def match(word)
+  word_frequency = Hash.new(0)
+  sentence = "super cat hair dog cattle catbird homework fan"
+  sentence.split.each do |dictionary|
+    word_frequency[/word/i.match(dictionary)] += 1
+  end
+  p word_frequency
+
+  #names = (regexp).names
+  #scan(regexp).collect do |match|
+  #Hash[names.zip(match)]
+  #{}/(cat)+/.match(word)[0]
+end
 
 end
--- spec/substrings_spec.rb
+++ spec/substrings_spec.rb
@@ -1,2 +1,19 @@
 require 'substrings'
 require 'spec_helper'
+
+describe Substrings do
+  let(:substrings) {Substrings.new}
+
+  it "can match one word exactly" do
+    expect(substrings.match("cat")).to eq "cat"
+  end
+
+  it "can match one word in a longer word" do
+    expect(substrings.match("catbird")).to eq "cat"
+  end
+
+  it "can match one word when it shows up twice" do
+    expect(substrings.match("cat cat")).to eq "cat cat"
+  end
+
+end
